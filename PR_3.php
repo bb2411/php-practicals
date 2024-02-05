fc30ceeed2a2b046f21181eba1fec5bfe6a2a1fc
bhargav bhatt <bhargavmbhatt24@gmail.com>
2024-02-05 22:05:45 +0530

    added practical file

---
+++ PR_3.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+<title>Practicaal 3</title>
+<?php
+$bap=$_POST['basic'];
+$da=(50/100)*$bap;
+$med=(4/100)*$bap;
+$hra=(10/100)*$bap;
+$gs=$bap+$da+$hra+$med;
+$ins=(7/100)*$gs;
+$pf=(5/100)*$gs;
+$de=$ins+$pf;
+$ns=$gs-$de;
+echo '<table>'
+    .'<tr><th>NAME</th><th>AMOUNT</th></tr>'
+    .'<tr><td>Net salary</td><td>'.$ns.'</td></tr>'
+    .'<tr><td>Gross salary</td><td>'.$gs.'</td></tr>'
+    .'<tr><td>Deduction</td><td>'.$de.'</td></tr>'
+    .'<tr><td>Dearness Allownce</td><td>'.$da.'</td></tr>'
+    .'<tr><td>Provinent Fund</td><td>'.$ns.'</td></tr>'
+    .'</table>';
+
+
+?>
--- arth.php
+++ arth.php
@@ -1,9 +1,17 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 <?php
-echo $_POST['op'];
 if($_POST['op']==='add'){
     echo "addition :".$_POST['a']+$_POST['b'];
+}else if($_POST['op']==='div'){
+    if($_POST['b']==0){
+        echo "division by zero";
+    }else{
+        echo "division :".$_POST['a']/$_POST['b'];
+    }
+}else if($_POST['op']==='mul'){
+    echo "multiplication :".$_POST['a']*$_POST['b'];
 }else{
-    echo "subtract :".$_POST['a']-$_POST['b'];
+    echo "subtraction :".$_POST['a']-$_POST['b'];   
 }
+echo "<li><a href='form.html'>go back</a></li>"
 ?>
