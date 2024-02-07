36bae6c0e71b0acf0f1848c174360caf9ca69a69
bhargav bhatt <bhargavmbhatt24@gmail.com>
2024-02-07 21:26:46 +0530

    added practicals

---
+++ student.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo "name :".$_POST['name']."<br>";
+echo "enrollment :".$_POST['enroll']."<br>";
+echo "semester :".$_POST['semester']."<br>";
+echo "email :".$_POST['email']."<br>";
+echo "department :".$_POST['dept']."<br>";
+
+?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
